d74f385e43b03f0c19df261f461f58865b81140d
shk33 <m.coronel.seg@gmail.com>
2015-01-25 18:46:58 -0600

    Add home page test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get principal home page" do
+    get :index
+    assert_response :success
+    assert_template 'index'
+    assert_select "title", 'Cadena de Favores'
+  end
+
 end
