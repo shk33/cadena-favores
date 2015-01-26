3e15736c9519e34f0e37d9b342cb954c0f6fbaf3
shk33 <m.coronel.seg@gmail.com>
2015-01-26 17:10:49 -0600

    Add create and destroy logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,34 @@
 class SessionsController < ApplicationController
   layout "site/site"
-  
+
   def new
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    
+    if @user && @user.authenticate(params[:session][:password])
+      #redirct the user to show profile 
+      if @user.activated?
+        login @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
+    else
+      #Create an error mesage
+      flash.now[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
   end
 
   def destroy
+    logout if logged_in?
+    redirect_to root_url
   end
 
 end
