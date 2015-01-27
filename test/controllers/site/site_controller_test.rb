35a645f16f0ead893c59a10eda17f13511c28adc
shk33 <m.coronel.seg@gmail.com>
2015-01-26 23:18:27 -0600

    Add test to count signup and login links

--- test/controllers/site/site_controller_test.rb
+++ test/controllers/site/site_controller_test.rb
@@ -6,6 +6,8 @@ class Site::SiteControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_template 'index'
+    assert_select 'a[href=?]', signup_path, text: 'Regístrate', count: 4
+    assert_select 'a[href=?]', login_path,  text: 'Ingresar',   count: 1
     assert_select "title", 'Cadena de Favores'
   end
 
