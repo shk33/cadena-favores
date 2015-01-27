5f0feba59f4e71a6f3c236bf166ca0456758c953
shk33 <m.coronel.seg@gmail.com>
2015-01-27 00:14:04 -0600

    Add signup test when not logged in

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,19 +11,6 @@ class UsersControllerTest < ActionController::TestCase
     # assert_not_nil assigns(:users)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create user" do
-    # assert_difference('User.count') do
-    #   post :create, user: { email: @user.email, name: @user.name, password_digest: @user.password_digest, remember_digest: @user.remember_digest }
-    # end
-
-    # assert_redirected_to user_path(assigns(:user))
-  end
-
   test "should show user" do
     # get :show, id: @user
     # assert_response :success
@@ -39,11 +26,4 @@ class UsersControllerTest < ActionController::TestCase
     # assert_redirected_to user_path(assigns(:user))
   end
 
-  test "should destroy user" do
-    # assert_difference('User.count', -1) do
-    #   delete :destroy, id: @user
-    # end
-
-    # assert_redirected_to users_path
-  end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,6 +6,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
+  ##########################################
+  # Signups should be invalid when logged in
+  ##########################################
+
   test "should signup redirect to main app when logged in" do
     log_in_as @user
     get signup_path
@@ -23,6 +27,49 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_response :redirect
     assert_redirected_to root_url
+  end
+
+  #######################################
+  # Signup when not logged in
+  #######################################
+  test "should reject signup invalid information" do
+    get signup_path
+    assert_response :success
+    assert_no_difference 'User.count' do
+      post signup_path, user: { name:  "V",
+                                 email: ".com",
+                                 password:              "123456",
+                                 password_confirmation: "123456" }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+  end
+
+  test "should reject signup with repeated email" do
+    get signup_path
+    assert_response :success
+    assert_no_difference 'User.count' do
+      post signup_path, user: { name:  "Valid Name",
+                                 email: "main_user@example.com",
+                                 password:              "123456",
+                                 password_confirmation: "123456" }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_response :success
+    assert_difference 'User.count', 1 do
+      post signup_path, user: { name:  "Valid Name",
+                                 email: "valid@valid.com",
+                                 password:              "123456",
+                                 password_confirmation: "123456" }
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_template 'home/index'
   end
 
 end
