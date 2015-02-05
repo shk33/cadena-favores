2171aaae6eb52652c24f37eb8b66155eb69b7d7c
shk33 <m.coronel.seg@gmail.com>
2015-02-04 19:58:20 -0600

    Create Tag Model and add references to Profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,5 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   has_one :pictures, as: :imageable
+  has_one :tags,     as: :taggable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205015151) do
+ActiveRecord::Schema.define(version: 20150205015632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,17 @@ ActiveRecord::Schema.define(version: 20150205015151) do
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tags", ["taggable_id", "taggable_type"], name: "index_tags_on_taggable_id_and_taggable_type", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"
