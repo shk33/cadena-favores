a64efd621b4a327bdb4bcfc9f07f0b0f74bd62e4
shk33 <m.coronel.seg@gmail.com>
2015-02-05 17:48:58 -0600

    Add followers and following relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,16 @@ class User < ActiveRecord::Base
   has_many  :reviews
   has_many  :chat_rooms
   has_many  :chats, through: :chat_rooms
+  has_many  :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+
+  has_many  :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+
+  has_many  :following, through: :active_relationships, source: :followed
+  has_many  :followers, through: :passive_relationships, source: :follower
   #Callbacks
   before_save   :downcase_email
 
