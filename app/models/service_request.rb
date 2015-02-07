c411063049e76c20894c1f634fe059cdd13d29db
shk33 <m.coronel.seg@gmail.com>
2015-02-07 12:07:19 -0600

    Agrega comentarios

--- app/models/service_request.rb
+++ app/models/service_request.rb
@@ -1,5 +1,5 @@
 class ServiceRequest < ActiveRecord::Base
-  belongs_to :user
-  has_one    :service, as: :serviceable
-  has_many   :offers
+  belongs_to :user #Obligatorio 
+  has_one    :service, as: :serviceable #Obligatorio
+  has_many   :offers #NO obligatorio
 end
