5c41279fa279775c85deef3147f5eda94b488ca5
shk33 <m.coronel.seg@gmail.com>
2015-02-12 01:21:04 -0600

    Creating a new relationship it creates an activity

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,40 @@
 class RelationshipsController < ApplicationController
+  before_action :set_relationship, only: [:show,  :destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  #Lejos de su implementación final.
+  #Implementación sólo para comprobar funcionamiento de notifiaciones
+  def create
+    @relationship = Relationship.new(relationship_params)
+
+    if @relationship.save
+      follower  = current_user
+      following = User.find(params[:followed_id])
+      @relationship.create_activity action: 'new', recipient: following, owner: follower
+      #redirect_to @recipe, notice: "Comment was created."
+    else
+      #render :new
+    end
+    redirect_to root_url
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_relationship
+      @relationship = Relationship.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def relationship_params
+      params.require(:relationship).permit()
+    end
 end
