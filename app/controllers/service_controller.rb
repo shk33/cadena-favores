502650295d70ee30a17e867c49c0c1a64b1d75a2
anuarml <anuar_ml93@hotmail.com>
2015-02-11 20:48:40 -0600

    SOl-04 Terminado de crear el create con la definicion de los params, creada la primera fase de la vista del service_request

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -3,8 +3,19 @@ class ServiceController < ApplicationController
   end
 
   def new
+  	@service_request = service_request.new
   end
 
   def create
+  	@service_request = service_request.new(service_request_params)
   end
+
+  private
+	  def service_request_params
+	  	params.require(:service_request).permit( :service_request_id, {service_attributes: 
+	  											[:title, 
+	  											 :description, 
+	  											 :cost])
+	  end
+
 end
--- app/models/service_request.rb
+++ app/models/service_request.rb
@@ -3,4 +3,5 @@ class ServiceRequest < ActiveRecord::Base
   has_one    :service, as: :serviceable #Obligatorio
   has_many   :offers #NO obligatorio
   has_and_belongs_to_many :tags #No obligatorio
+  accepts_nested_attributes_for :service
 end
