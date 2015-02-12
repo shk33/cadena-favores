5c754cd49f09484d5665eb03d70d24bb9ba8a356
shk33 <m.coronel.seg@gmail.com>
2015-02-12 13:11:58 -0600

    Add tests for activities index

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,6 @@
 class ActivitiesController < ApplicationController
-
+  before_action :logged_in_user
+  
   def index
     @activities = PublicActivity::Activity.order("created_at desc")
                   .where(recipient_id: current_user, recipient_type: "User")
--- test/controllers/activities_controller_test.rb
+++ test/controllers/activities_controller_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class ActivitiesControllerTest < ActionController::TestCase
+  setup do
+    @user = users(:main_user)
+    log_in_as @user
+  end
+
   test "should get index" do
     get :index
+    assert_select 'h2', "Notificaciones"
     assert_response :success
   end
 
