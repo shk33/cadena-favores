9c77156c3f05a951e3962463a941094f3ed78363
shk33 <m.coronel.seg@gmail.com>
2015-02-13 20:15:27 -0600

    Test service request controller new

--- test/controllers/service_requests_controller_test.rb
+++ test/controllers/service_requests_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class ServiceRequestsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    log_in_as @user
+  end
+
+  #Pruebo que la vista de 
+  test "should get new" do
+    get :new
+    #Verifico que renderiza la vista new
+    assert_template :new
+    #Verifico que están los labels de la vista
+    assert_select   'label', 'Título'
+    assert_select   'label', 'Descripción'
+    assert_select   'label', 'Costo en Puntos'
+    #Verifico la existencia de los inputs
+    assert_select "input", :name  => "service_request[service_attributes][title]"
+    assert_select "input", :name  => "service_request[service_attributes][description]"
+    assert_select "input", :name  => "service_request[service_attributes][cost]"
+    assert_select "input", :value => "Crear"
+
+  end
 end
