efd328c8ba4d15c6fdeeeaa1eabc3907f33977fd
shk33 <m.coronel.seg@gmail.com>
2015-02-13 19:05:06 -0600

    Add owner to ServiceRequest

--- app/controllers/service_requests_controller.rb
+++ app/controllers/service_requests_controller.rb
@@ -1,11 +1,13 @@
 class ServiceRequestsController < ApplicationController
+before_action :logged_in_user
 
 def new
 	@service_request = ServiceRequest.new
 end
 
 def create
-	@service_request = ServiceRequest.new(service_request_params)
+  @service_request = ServiceRequest.new(service_request_params)
+	@service_request.user = current_user
 
 	respond_to do |format|
 		if @service_request.save
