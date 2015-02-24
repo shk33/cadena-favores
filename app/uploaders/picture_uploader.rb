c57800f44b4dac2d200428f6adcda3d1f94ddfc4
shk33 <m.coronel.seg@gmail.com>
2015-02-24 12:24:43 -0600

    Configure uploading

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,6 +1,14 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [90, 80]
+
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
