c5a4df52b24acb73087b494630e0fc30733b7939
shk33 <m.coronel.seg@gmail.com>
2015-02-28 23:59:32 -0600

    Display in Top Header amout of new notifications Complete Not-01 Borrar Notificación

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,5 +4,9 @@ class Activity < ActiveRecord::Base
     PublicActivity::Activity.order("created_at desc")
                   .where(recipient_id: user, recipient_type: "User").limit(5)
   end
+
+  def self.count_new_notifications user
+    PublicActivity::Activity.where(recipient_id: user, recipient_type: "User", read: false).count
+  end
   
 end
