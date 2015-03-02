e51e3fb0db6bc0612e53b2fc20e3637398e19280
shk33 <m.coronel.seg@gmail.com>
2015-03-02 15:41:51 -0600

    Verifies tags_ids when update a profile

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -9,6 +9,8 @@ class ProfilesControllerTest < ActionController::TestCase
   test "should edit profiles tags" do
     tags = [ Tag.first.id , Tag.second.id ]
     patch :update, user_id: @user, id: @user.profile, profile: { tag_ids: tags }
+    profile = assigns :profile
+    assert_equal profile.tag_ids, tags
     assert_redirected_to @user
   end
 end
