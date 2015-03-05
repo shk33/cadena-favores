4aa26cf2b7a3854d512d87a5ed499959e078ccb9
shk33 <m.coronel.seg@gmail.com>
2015-03-05 09:28:27 -0600

    Add service arrengement model tests

--- test/models/service_arrangement_test.rb
+++ test/models/service_arrangement_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class ServiceArrangementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do 
+    @arrangements = service_arrangements :one
+  end
+
+  test "should be valid" do
+    assert @arrangements.valid?
+  end
+
+  test "should have a client" do
+    @arrangements.client = nil
+    assert_not @arrangements.valid?
+  end
+
+  test "should have a server" do
+    @arrangements.server = nil
+    assert_not @arrangements.valid?
+  end
+
+  test "should have a end_date" do
+    @arrangements.end_date = nil
+    assert_not @arrangements.valid?
+  end
+
+  test "should have a start_date" do
+    @arrangements.start_date = nil
+    assert_not @arrangements.valid?
+  end
+
+  test "should have an offer" do
+    @arrangements.offer = nil
+    assert_not @arrangements.valid?
+  end
+
 end
