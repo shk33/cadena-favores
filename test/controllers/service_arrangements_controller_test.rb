8cfdf12e1f74f498460660eb8b044e4673ca3694
anuarml <anuar_ml93@hotmail.com>
2015-03-08 00:13:10 -0600

    GSR-06 Finalizado con pruebas

--- test/controllers/service_arrangements_controller_test.rb
+++ test/controllers/service_arrangements_controller_test.rb
@@ -31,5 +31,51 @@ class ServiceArrangementsControllerTest < ActionController::TestCase
     post :update, id: @arrangement
     assert_redirected_to my_hired_completed_url
   end
+  
+   test "should show a valid service_arrangement" do
+    #NO ENTIENDO PORQUE NO RECONOCE EL START_DATE Y EL END_DATE SI EL FIXTURE Y ESO ESTA BIEN
+    service_request = service_requests(:one)
+    service_arrangement = service_arrangements(:one)
+    service = service_request.service
+    get :show, id: service_arrangement
+    assert_template :show
+    assert_response :success
+    assert_select 'h2', 'Detalles del servicio a realizar'
+    assert_select 'label', 'Servidor'
+    assert_select 'p', service_arrangement.server.name
+    assert_select 'label', 'Fecha de inicio'
+    #assert_select 'p', service_arrangement.start_date
+    assert_select 'label', 'Fecha de fin'
+    #assert_select 'p', service_arrangement.end_date
+    assert_select 'a', service.title
+    assert_select 'span', "#{service.cost} puntos"
+    assert_select 'p', service.description
+    service_request.tags.each do |tag|
+        assert_select 'span', tag.name
+    end
+  end
+
+  test "should get index" do
+    arrangements = ServiceArrangement.all
+    get :index
+    assert_response :success
+    assert_select 'h2', 'Servicios a realizar'
+    #NO SE COMO SACAR EL SERVICIO DEL ARRANGEMENT LO PUSE ASI PORQUE ASI ESTA EN LA VISTA DE LIST
+    arrangements.each do |arrangement|
+        assert_select 'strong', arrangement.service.title
+        assert_select 'p', arrangement.service.description
+        assert_select 'strong', 'Usuario Contratado:'
+        assert_select 'strong', 'Fecha de Inicio::'
+        assert_select 'p', arrangement.start_date
+        assert_select 'strong', 'Fecha de Entrega:'
+        assert_select 'p', arrangement.end_date
+        assert_select 'strong', 'Costo: '
+        assert_select 'div', "#{arrangement.service.cost} puntos"
+        assert_select 'label', tag.name
+        assert_select "input", :type => "submit", :value => "Aceptar como Concluido"
+        assert_select "input", :value => "Cancelar"
+    end
+
+  end
 
 end
