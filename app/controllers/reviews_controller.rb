9b3aa7696f12a6ac7ef278a0a291b7e7ba0282e8
shk33 <m.coronel.seg@gmail.com>
2015-03-12 12:03:07 -0600

    Create a new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,33 @@
 class ReviewsController < ApplicationController
   before_action :logged_in_user, :get_notifications
-  before_action :set_service_arrangment, only: [:new]
+  before_action :set_service_arrangment, only: [:new, :create]
 
   def new
     @review = @arrangement.build_review
     redirect_to root_url unless @arrangement.client == current_user
   end
 
+  def create
+    @review = Review.new review_params
+    @review.service_arrangement = @arrangement
+    @review.user = current_user
+    @arrangement.review = @review
+    if @arrangement.save
+      flash[:success] = "Servico Calificado Exitósamente"
+      redirect_to @arrangement
+    else
+      render :new    
+    end
+  end
+
   private
     def set_service_arrangment
       @arrangement = ServiceArrangement.find(
                                     params[:service_arrangement_id])
+    end
+
+    def review_params
+      params.require(:review).permit(:rating, :description)
     end
   
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,6 @@ class Review < ActiveRecord::Base
   #Model Validations
   validates :rating, :inclusion => 1..5, presence: true
   validates :description, presence: true
-  validates :service_arrangement
-  validates :user
+  validates :service_arrangement, presence: true
+  validates :user, presence: true
 end
