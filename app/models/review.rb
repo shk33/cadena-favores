a688c59e3f7dc799b7d6c91290bec51c26b883a8
shk33 <m.coronel.seg@gmail.com>
2015-03-12 10:44:20 -0600

    Add review model validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :service_arrangement
+
+  #Model Validations
+  validates :rating, :inclusion => 1..5, presence: true
+  validates :description, presence: true
+  validates :service_arrangement
+  validates :user
 end
