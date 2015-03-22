ea913eff31c866e8ba625e853da8b40de6fbc21c
shk33 <m.coronel.seg@gmail.com>
2015-03-22 16:58:54 -0600

    Remove unused profile search action

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,14 +5,6 @@ class Profile < ActiveRecord::Base
 
   validate  :picture_size
 
-  def self.search_by_tag tag_id
-    if tag_id
-      Profile.joins(:tags).where(tags: {id: tag_id.to_i})
-    else
-      Profile.all
-    end
-  end
-
   private
     # Validates the size of an uploaded picture.
     def picture_size
