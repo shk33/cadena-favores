fdbee2f131631f5e83dfd4741d866574bead7f27
Miguel Coronel <m.coronel.seg@gmail.com>
2015-04-13 10:39:36 -0500

    Fix 'undefined relation Pictures' error by adding Pictures Table. Its necessary to find out which fixture still referencing that table

--- db/migrate/20150224172010_drop_pictures_table.rb
+++
@@ -1,5 +0,0 @@
-class DropPicturesTable < ActiveRecord::Migration
-  def change
-    drop_table :pictures
-  end
-end
---
+++ db/migrate/20150413152246_add_picture_table.rb
@@ -0,0 +1,10 @@
+class AddPictureTable < ActiveRecord::Migration
+  def change
+    create_table :pictures do |t|
+      t.references :imageable, polymorphic: true, index: true
+      t.text :route
+      t.string :name
+      t.timestamps
+    end
+  end
+end
